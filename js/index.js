f8c3357b4ccec3d505a2996ab5beecdf9a2f15af
larryvsdata <larryvsdata@gmail.com>
2019-01-18 21:29:37 -0800

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,76 @@
 // your code here
+
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
+}
+
+function displayRepositories() {
+  var repos = JSON.parse(this.responseText);
+  console.log(repos);
+  const repoList = `<ul>${repos
+    .map(
+      r =>
+        '<li>' +
+        r.html_url +
+        ' - <a href="#" data-repo="' +
+        r.name + '"data-username"' + r.owner.login +
+        '" onclick="getCommits(this)">Get Commits</a></li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('repositories').innerHTML = repoList;
+}
+
+
+function displayCommits(){
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><strong>' +
+        commit.author.login +
+        '</strong> - ' +
+        commit.commit.author.name +
+        '</strong> - ' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('details').innerHTML = commitsList;
+
+}
+
+
+function getCommits(el) {
+  const name = el.dataset.repository;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayCommits);
+  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
+  req.send();
+}
+
+function getBranches(el) {
+  const name = el.dataset.repository;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayBranches);
+  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/branches');
+  req.send();
+}
+
+
+function displayBranches() {
+  var branches = JSON.parse(this.responseText);
+
+  const branchesList = `<ul>${branches
+    .map(
+      b =>
+        '<li>' +
+        b.name +
+        ' - ' +
+        b.commit.url +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('details').innerHTML = branchesList;
+}
